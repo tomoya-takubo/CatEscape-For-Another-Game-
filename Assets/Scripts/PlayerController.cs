49145a9888af11e75c87c35f770fd8cc5b266128
tomoya-takubo <tkb1003@gmail.com>
2020-03-23 12:22:49 +0900

    20200323　コミット（2回目)
    ～矢当たり判定まで実装

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //左矢印が押されたとき
+        if (Input.GetKeyDown(KeyCode.LeftArrow))
+        {
+            transform.Translate(-3, 0, 0);  //左に[3]動かす
+        }
+
+        //右矢印が押されたとき
+        if (Input.GetKeyDown(KeyCode.RightArrow))
+        {
+            transform.Translate(3, 0, 0);   //右に[3]動かす
+        }
+    }
+}
